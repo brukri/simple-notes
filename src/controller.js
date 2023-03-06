58a0fb7c94af2ad25402b5660a6703245da7a711
NinoHelfenstein <nino.helfenstein@hispeed.ch>
2023-03-06 16:00:58 +0100

    addNote()

--- src/controller.js
+++ src/controller.js
@@ -1,137 +1,152 @@
-const SIMPLE_NOTES_STORAGE_KEY = 'simpleNodes';
+const SIMPLE_NOTES_STORAGE_KEY = "simpleNodes";
 
 const controller = (() => {
-    function getAndClearInputNoteTitle() {
-        const element = document.getElementById("input-add-note-title")
-        const value = element.value;
-        element.value = '';
-        return value;
+  function getAndClearInputNoteTitle() {
+    const element = document.getElementById("input-add-note-title");
+    const value = element.value;
+    element.value = "";
+    return value;
+  }
+
+  function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+  }
+
+  function getAndClearTextareaNoteText() {
+    const element = document.getElementById("textarea-add-note-text");
+    const value = element.value;
+    element.value = "";
+    return value;
+  }
+
+  function createNote({ noteId, title, text }) {
+    const existingNotes = document.getElementById("flex-wrapper");
+    const existingNoteTemplate = document.getElementById(
+      "existing-note-template"
+    );
+    const newNode = existingNoteTemplate.cloneNode(true);
+    newNode.removeAttribute("hidden");
+    const elementId = `existing-node-${noteId}`;
+    newNode.setAttribute("id", elementId);
+    existingNotes.appendChild(newNode);
+    document.querySelector(`#${elementId} #existing-note-title`).innerText =
+      title;
+    document.querySelector(`#${elementId} #existing-note-text`).innerText =
+      text;
+    document
+      .querySelector(`#${elementId} #existing-note-delete-button`)
+      .setAttribute("onclick", `controller.deleteNote('${noteId}')`);
+    document
+      .querySelector(`#${elementId} #existing-note-edit-button`)
+      .setAttribute("onclick", `controller.editNote('${noteId}')`);
+  }
+
+  function addNoteToLocalStorage(note) {
+    let notes = JSON.parse(localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY));
+    notes.push(note);
+    localStorage.setItem(SIMPLE_NOTES_STORAGE_KEY, JSON.stringify(notes));
+  }
+
+  function deleteNoteFromLocalStorage(id) {
+    let notes = JSON.parse(localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY));
+    const foundIndex = notes.findIndex((note) => note.noteId === id);
+    notes.splice(foundIndex, 1);
+    localStorage.setItem(SIMPLE_NOTES_STORAGE_KEY, JSON.stringify(notes));
+  }
+
+  function setInNoteFromLocalStorage(id) {
+    let notes = JSON.parse(localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY));
+    const foundIndex = notes.findIndex((note) => note.noteId === id);
+    const foundNote = notes[foundIndex];
+    document.getElementById("input-add-note-title").value = foundNote.title;
+    document.getElementById("textarea-add-note-text").value = foundNote.text;
+    document.getElementById("edit-note-button").style.display = "block";
+    document
+      .getElementById("edit-note-button")
+      .setAttribute("onclick", `controller.updateNote('${id}')`);
+    document.getElementById("add-note-button").style.display = "none";
+  }
+
+  function updateNoteToLocalStorage(noteId) {
+    let notes = JSON.parse(localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY));
+    const foundIndex = notes.findIndex((note) => note.noteId === noteId);
+    const title = getAndClearInputNoteTitle();
+    const text = getAndClearTextareaNoteText();
+
+    if (!title || !text) {
+      return;
     }
 
-    function toggleTheme() {
-        document.body.classList.toggle("dark-mode");
+    notes[foundIndex] = { noteId, title, text };
+    localStorage.setItem(SIMPLE_NOTES_STORAGE_KEY, JSON.stringify(notes));
+    document.getElementById("edit-note-button").style.display = "none";
+    document.getElementById("add-note-button").style.display = "block";
+  }
+
+  function addNote() {
+    const title = getAndClearInputNoteTitle();
+    const text = getAndClearTextareaNoteText();
+
+    if (!title || !text) {
+      return;
+    }
+    autoResize();
+
+    const noteId = new Date().getTime().toString();
+
+    addNoteToLocalStorage({ noteId, title, text });
+    loadNotes();
+  }
+
+  function deleteNote(noteId) {
+    deleteNoteFromLocalStorage(noteId);
+    loadNotes();
+  }
+  function editNote(noteId) {
+    setInNoteFromLocalStorage(noteId);
+    autoResize();
+  }
+
+  function updateNote(noteId) {
+    updateNoteToLocalStorage(noteId);
+    loadNotes();
+    autoResize();
+  }
+
+  function loadNotes() {
+    const elements = document.getElementsByClassName("notes-container");
+    console.log(elements)
+    for(let i = 1; i < elements.length; i++){
+        elements[0].parentNode.removeChild(elements[i]);
+        console.log("deleted", i)
+    }
+    const notesString = localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY);
+
+    if (!notesString) {
+      localStorage.setItem(SIMPLE_NOTES_STORAGE_KEY, JSON.stringify([]));
     }
 
-    function getAndClearTextareaNoteText() {
-        const element = document.getElementById("textarea-add-note-text")
-        const value = element.value;
-        element.value = '';
-        return value;
+    const notes = JSON.parse(notesString);
+    for (let i = 0; i < notes.length; i++) {
+        createNote(notes[i]);
     }
+  }
 
-    function createNote({ noteId, title, text }) {
-        const existingNotes = document.getElementById("div-existing-notes");
-        const existingNoteTemplate = document.getElementById("existing-note-template")
-        const newNode = existingNoteTemplate.cloneNode(true);
-        newNode.removeAttribute("hidden");
-        const elementId = `existing-node-${noteId}`;
-        newNode.setAttribute("id", elementId);
-        existingNotes.appendChild(newNode);
-        document.querySelector(`#${elementId} #existing-note-title`).innerText = title;
-        document.querySelector(`#${elementId} #existing-note-text`).innerText = text;
-        document.querySelector(`#${elementId} #existing-note-delete-button`).setAttribute("onclick", `controller.deleteNote('${noteId}')`);
-        document.querySelector(`#${elementId} #existing-note-edit-button`).setAttribute("onclick", `controller.editNote('${noteId}')`);
-    }
+  function autoResize() {
+    const textarea = document.getElementById("textarea-add-note-text");
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+    console.log("scrollHeight: " + textarea.scrollHeight);
+    console.log("clientHeight: " + textarea.clientHeight);
+  }
 
-    function addNoteToLocalStorage(note) {
-        let notes = JSON.parse(localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY));
-        notes.push(note)
-        localStorage.setItem(SIMPLE_NOTES_STORAGE_KEY, JSON.stringify(notes));
-    }
-
-    function deleteNoteFromLocalStorage(id) {
-        let notes = JSON.parse(localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY));
-        const foundIndex = notes.findIndex(note => note.noteId === id);
-        notes.splice(foundIndex, 1);
-        localStorage.setItem(SIMPLE_NOTES_STORAGE_KEY, JSON.stringify(notes));
-    }
-
-    function setInNoteFromLocalStorage(id) {
-        let notes = JSON.parse(localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY));
-        const foundIndex = notes.findIndex(note => note.noteId === id);
-        const foundNote = notes[foundIndex];
-        document.getElementById("input-add-note-title").value = foundNote.title;
-        document.getElementById("textarea-add-note-text").value = foundNote.text;
-        document.getElementById("edit-note-button").style.display = "block";
-        document.getElementById("edit-note-button").setAttribute("onclick", `controller.updateNote('${id}')`);
-        document.getElementById("add-note-button").style.display = "none";
-        }
-
-    function updateNoteToLocalStorage(noteId) {
-        let notes = JSON.parse(localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY));
-        const foundIndex = notes.findIndex(note => note.noteId === noteId);
-        const title = getAndClearInputNoteTitle();
-        const text = getAndClearTextareaNoteText();
-
-        if (!title || !text) {
-            return;
-        }
-
-        notes[foundIndex] = { noteId, title, text };
-        localStorage.setItem(SIMPLE_NOTES_STORAGE_KEY, JSON.stringify(notes));
-        document.getElementById("edit-note-button").style.display = "none";
-        document.getElementById("add-note-button").style.display = "block";
-
-    }
-    
-    function addNote() {
-        const title = getAndClearInputNoteTitle();
-        const text = getAndClearTextareaNoteText();
-        
-        if (!title || !text) {
-            return;
-        }
-        autoResize();
-        
-        const noteId = new Date().getTime().toString();
-        
-        addNoteToLocalStorage({ noteId, title, text });
-        loadNotes();
-    }
-
-    function deleteNote(noteId) {
-        deleteNoteFromLocalStorage(noteId);
-        loadNotes();
-    }
-    function editNote(noteId) {
-        setInNoteFromLocalStorage(noteId);
-        autoResize();
-    }
-
-    function updateNote(noteId) {
-        updateNoteToLocalStorage(noteId);
-        loadNotes();
-        autoResize();
-    }
-    
-    function loadNotes() {
-        document.getElementById("div-existing-notes").innerHTML = '';
-        const notesString = localStorage.getItem(SIMPLE_NOTES_STORAGE_KEY);
-        
-        if (!notesString) {
-            localStorage.setItem(SIMPLE_NOTES_STORAGE_KEY, JSON.stringify([]));
-        }
-
-        const notes = JSON.parse(notesString);
-        notes.forEach(createNote);
-    }
-
-    function autoResize() {
-        const textarea = document.getElementById("textarea-add-note-text");
-        textarea.style.height = "auto";
-        textarea.style.height = textarea.scrollHeight + "px";
-        console.log("scrollHeight: " + textarea.scrollHeight);
-        console.log("clientHeight: " + textarea.clientHeight);
-      }
-
-    return {
-        addNote,
-        deleteNote,
-        loadNotes,
-        editNote,
-        toggleTheme,
-        updateNote,
-        autoResize
-    }
-
+  return {
+    addNote,
+    deleteNote,
+    loadNotes,
+    editNote,
+    toggleTheme,
+    updateNote,
+    autoResize,
+  };
 })();
